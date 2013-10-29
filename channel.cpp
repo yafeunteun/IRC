285002f448fda419a793db4215152eb7d96433a6
yafeunteun <yannfeunteun@gmail.com>
2013-10-29 20:43:27 +0100

    Channel now have 3 list of clients : - m_banList : for clients who are banned in this channel. - m_clientList : for clients who are regular i.e who are neither banned nor operator. - m_operatorList : for clients who are operator in this channel.
    As a result the following methods have been changed :
    - Channel::addClient
    - Channel::getClientList

--- channel.cpp
+++ channel.cpp
@@ -1,30 +1,68 @@
 #include "channel.h"
+#include "bdPlatformLog.h"
 
-Channel::Channel(const QString &name): channel_name(name)
+Channel::Channel(const QString &name): m_channel_name(name)
 {
 }
 
 void Channel::setTopic( const QString& _topic )
 {
-    topic = _topic;
+    m_topic = _topic;
 }
 
 QString& Channel::getTopic( void )
 {
-    return topic;
+    return m_topic;
 }
 
 QString& Channel::getChannelName( void )
 {
-    return channel_name;
+    return m_channel_name;
 }
 
-std::list<Client*>& Channel::getClientList( void )
+std::list<Client*>& Channel::getClientList( status s )
 {
-    return clientList;
+    switch (s) {
+    case BANNED:
+        return m_banList;
+        break;
+    case REGULAR:
+        return m_clientList;
+        break;
+    case OPERATOR:
+        return m_operatorList;
+        break;
+    default:
+        bdPlatformLog::bdLogMessage(_ERROR, "fatal_error/", "channel", __FILE__, __PRETTY_FUNCTION__, __LINE__, "Couldn't evaluate second argument wich must be of type status (see channel.h)");
+        exit(-1);
+    }
 }
 
-void Channel::addClient( Client *c )
+void Channel::addClient(Client *c , status s)
 {
-    clientList.push_front(c);
+    switch (s) {
+    case BANNED:
+        m_banList.push_front(c);
+        break;
+    case REGULAR:
+        m_clientList.push_front(c);
+        break;
+    case OPERATOR:
+        m_operatorList.push_front(c);
+        break;
+    default:
+        bdPlatformLog::bdLogMessage(_ERROR, "fatal_error/", "channel", __FILE__, __PRETTY_FUNCTION__, __LINE__, "Couldn't evaluate second argument wich must be of type status (see channel.h)");
+        exit(-1);
+    }
 }
+
+ bool Channel::isBanned(Client * c)
+ {
+     for(std::list<Client*>::iterator it = m_banList.begin(); it != m_banList.end(); ++it)
+     {
+         if((*it)->getNickname() == c->getNickname())
+             return true;
+     }
+
+     return false;
+ }
--- channel.h
+++ channel.h
@@ -7,12 +7,16 @@
 
 class Client;
 
+typedef enum {BANNED, REGULAR, OPERATOR} status;
+
+
 class Channel
 {
 private:
-    std::list<Client*> clientList;
-    std::list<Client*> operatorList;
-    QString topic, channel_name;
+    std::list<Client*> m_clientList;
+    std::list<Client*> m_operatorList;
+    std::list<Client*> m_banList;
+    QString m_topic, m_channel_name;
 
 public:
     Channel( const QString& name );
@@ -21,8 +25,9 @@ public:
     void setTopic(const QString &_topic );
     QString& getTopic( void );
     QString& getChannelName( void );
-    std::list<Client*>& getClientList( void );
-    void addClient( Client *c );
+    std::list<Client*>& getClientList( status s = REGULAR );
+    void addClient( Client *c, status s = REGULAR );
+    bool isBanned(Client * c);
 };
 
 #endif // CHANNEL_H
