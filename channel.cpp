e20668774145c5d5a093c01522f5b0e58ca2298f
yafeunteun <yannfeunteun@gmail.com>
2013-11-10 22:04:59 +0100

    Class Channel comments have been set to generate documentation with Doxygen.

--- channel.cpp
+++ channel.cpp
@@ -1,25 +1,61 @@
 #include "channel.h"
 #include "bdPlatformLog.h"
 
+
+
+/*!
+*  \brief Constructor
+*
+*  \param name : Name of the channel.
+*/
 Channel::Channel(const QString &name): m_channel_name(name)
 {
 }
 
-void Channel::setTopic( const QString& _topic )
+/*!
+*  \brief Set the topic of the channel.
+*
+*  \param topic : A string containing the topic of the channel.
+*  \return void.
+*/
+void Channel::setTopic( const QString& topic )
 {
-    m_topic = _topic;
+    m_topic = topic;
 }
 
+
+/*!
+*  \brief Getter of the topic of the channel.
+*
+*  \return A string containing the topic of the channel.
+*/
 QString& Channel::getTopic( void )
 {
     return m_topic;
 }
 
+/*!
+*  \brief Getter of the name of the channel.
+*
+*  \return A string containing the name of the channel.
+*/
 QString& Channel::getChannelName( void )
 {
     return m_channel_name;
 }
 
+
+/*!
+*  \brief Getter of the client lists of the channel.
+*
+*  For example : sampleChannel.getClientList(OPERATOR) returns the operator clients list of the channel.
+*
+*  If no status is given, the getter returns the connected clients list.
+*
+*  \param s : The client list you want to get
+*  \return A list (from STL) containing the adresses of clients depending on the status given in parameter.
+*
+*/
 std::list<Client*>& Channel::getClientList( status s )
 {
     switch (s) {
@@ -38,6 +74,15 @@ std::list<Client*>& Channel::getClientList( status s )
     }
 }
 
+
+
+/*!
+*  \brief Allows to add and/or set the status of a client on the channel.
+*
+*  \param c : The address of the client you want to add and or change the status on the channel.
+*  \param s : The status you want to give to the client you are adding.
+*  \return void.
+*/
 void Channel::addClient(Client *c , status s)
 {
     switch (s) {
@@ -58,12 +103,27 @@ void Channel::addClient(Client *c , status s)
 }
 
 
+/*!
+*  \brief Unban a client
+*
+*  \param c : The address of the client you want to unban.
+*  \return void.
+*/
 void Channel::unbanClient(Client *c)
 {
     m_banList.remove(c);
 }
 
-
+/*!
+*  \brief Remove a client from a channel.
+*
+*  Remove a client from the regular and operator lists if the client is operator on the channel.
+*  This function doesn't intend to remove a client from the ban list, if you want to do so,
+*  you should use unbanClient().
+*
+*  \param c : The address of the client you want to remove from the channel.
+*  \return void.
+*/
 void Channel::removeClient(Client *c)
 { 
     if(isStatus(c, OPERATOR))
@@ -72,8 +132,15 @@ void Channel::removeClient(Client *c)
 }
 
 
- bool Channel::isStatus(Client * c, status s)
- {
+/*!
+*  \brief Check the status of a client on a channel.
+*
+*  \param c : The address of the client you want to check the status.
+*  \param s : The status you want to check if the client has.
+*  \return True if the client c is in the list corresponding to status given, false either.
+*/
+bool Channel::isStatus(Client * c, status s)
+{
      switch (s) {
      case BANNED:
          for(std::list<Client*>::iterator it = m_banList.begin(); it != m_banList.end(); ++it)
@@ -103,9 +170,4 @@ void Channel::removeClient(Client *c)
          bdPlatformLog::bdLogMessage(_ERROR, "fatal_error/", "channel", __FILE__, __PRETTY_FUNCTION__, __LINE__, "Couldn't evaluate second argument wich must be of type status (see channel.h)");
          exit(-1);
      }
-
-
-
-
-
- }
+}
--- channel.h
+++ channel.h
@@ -1,35 +1,62 @@
 #ifndef CHANNEL_H
 #define CHANNEL_H
 
+
+/*!
+ * \file channel.h
+ * \brief This file gathers tools to manage clients inside channels.
+ * \author yann feunteun
+ */
+
+
 #include <list>
 #include <iostream>
 #include "client.h"
 
 class Client;
 
-typedef enum {BANNED, REGULAR, OPERATOR} status;
+
+/**
+ * \enum status
+ * \brief Status of a client on the channel
+ */
+typedef enum
+{
+    BANNED,     /*!< Client is banned of the channel */
+    REGULAR,    /*!< Client is connected to the channel */
+    OPERATOR    /*!< Client is operator on the channel note that it means the client is also connected to the channel */
+}
+status;
 
 
 class Channel
 {
+
+/*! \class Channel
+* \brief Class reprensenting a channel
+*
+*  This class manage 3 lists of clients : banned, regular (connected to the channel including operators) and operator clients.
+*/
+
 private:
-    std::list<Client*> m_clientList;        // contains both the regular clients and operators
-    std::list<Client*> m_operatorList;      // contains only operators of the channel
-    std::list<Client*> m_banList;           // contains the clients who are banned from the channel
-    QString m_topic, m_channel_name;
+    std::list<Client*> m_clientList;        /*!< Contains both the regular clients and operators */
+    std::list<Client*> m_operatorList;      /*!< Contains only operators of the channel */
+    std::list<Client*> m_banList;           /*!< Contains the clients who are banned from the channel */
+    QString m_topic;                        /*!< The topic of the channel */
+    QString m_channel_name;                 /*!< The name of the channel */
 
 public:
     Channel( const QString& name );
     ~Channel();
 
-    void setTopic(const QString &_topic );
+    void setTopic(const QString &topic );
     QString& getTopic( void );
     QString& getChannelName( void );
     std::list<Client*>& getClientList( status s = REGULAR );
     void addClient( Client *c, status s = REGULAR );
     void removeClient(Client *c);
     void unbanClient(Client *c);
-    bool isStatus(Client * c, status s = REGULAR);  // true if the client c is in the list corresponding to status given.
+    bool isStatus(Client * c, status s = REGULAR);
 };
 
 #endif // CHANNEL_H
